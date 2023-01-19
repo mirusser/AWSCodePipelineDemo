78e1159956dfbfe31097bb3a26a02cc760185c7d
mirusser <mirusser@gmail.com>
2023-01-19 14:30:37 +0100

    AWSCodePipelineDemo: fixes to Program.cs

--- AWSCodePipelineDemo/Program.cs
+++ AWSCodePipelineDemo/Program.cs
@@ -1,27 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
 
-{
-    builder.Services.AddControllers();
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
-}
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
-
-    app.UseHttpsRedirection();
-
-    app.UseAuthorization();
-
-    app.MapControllers();
-
-    app.MapGet("/", () => "Hello World!");
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapGet("/", () => "Hello World v2!");
 app.Run();
